32b4112043a8cae5eaa3512ff967dc6076c51c27
Johan Cañas <jxexcxo@gmail.com>
2021-01-14 13:01:50 -0500

    update account setup

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
-
+from flask_login import current_user
 class RegistrationForm(FlaskForm):
     username = StringField('Nombre de usuario', 
                             validators=[DataRequired(), Length(min=2, max=20)])
@@ -27,6 +28,29 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('Email',
                             validators=[DataRequired(), Email()])
-    password = PasswordField('Contrasela', validators=[DataRequired()])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
     remember = BooleanField('Recordarme')
     submit = SubmitField('Iniciar Sesión')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Nombre de usuario', 
+                            validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                            validators=[DataRequired(), Email()])
+
+    picture = FileField('Actualiza tu imagen de usuario', validators=[
+                            FileAllowed(['jpg', 'png'])
+    ])
+    submit = SubmitField('Actualizar')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Nombre de usuario ya existe')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('Dirección de email ya existe')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,9 @@
+import secrets
+import os
+from PIL import Image
 from flask import render_template, redirect, url_for, flash, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -67,7 +70,38 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+def save_picture(form_picture):
+    randox_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename) # return name file and extension file
+    picture_fn = randox_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    # resizing larte pictures
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Cuenta personal')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data 
+        db.session.commit()
+        flash('Tu cuenta fue actualizada', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        app.logger.debug(current_user.username, current_user.email)
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Cuenta personal', 
+                                        image_file=image_file, form=form)
+
